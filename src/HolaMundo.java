59b6580023c2b52a9ecc3b258fbacf58438acb28
acadenasg03 <acadenasg03@educarex.es>
2023-11-23 14:13:43 +0100

    Syso añadido

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -7,6 +7,8 @@ public class HolaMundo {
 		System.out.println("Hola Mundo");
 		System.out.println("Bienvenidos...");
 		System.out.println("La suma de 7 más 2 es igual a: " + (7+2));
+		
+		System.out.println("Álvaro Cadenas");
 
 	}
 
